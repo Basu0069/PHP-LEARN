95b26304f2c5d4896e18402dbd8217d26914c2b9
Basu Slathia <basuslathia01@gmail.com>
2025-02-12 21:36:20 +0530

    datatypes

---
+++ 003_datatypes.php
@@ -0,0 +1,31 @@
+<?php
+
+$name= "Anile Sidhu";
+// echo var_dump($name);
+
+
+
+$num=10;
+// echo var_dump($num);
+
+
+
+$numFloat= 10.10;
+// echo var_dump($numFloat);
+
+
+$bool= true;
+// echo var_dump($bool);
+
+$arr=["anil","sam",10];
+// echo var_dump($arr);
+
+$empty=null;
+echo var_dump($empty);
+
+
+
+
+
+
+?>
