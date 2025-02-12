b5a90b34977f60946a5f0e9d3814ba62199d75bc
Basu Slathia <basuslathia01@gmail.com>
2025-02-12 21:37:58 +0530

    operators

---
+++ 004_operators.php
@@ -0,0 +1,54 @@
+<?php
+// Arithetic operators::
+
+$a= 20;
+$b=10;
+echo $a+$b;
+// echo $a-$b;
+// echo $a*$b;
+// echo $a/$b;
+// echo $a%$b;
+// echo $a**$b;
+
+
+
+// const num1= 20;
+// const num2= 30;
+// echo num1*num2;
+
+?>
+
+
+
+<?php 
+// Assignment operator:
+// +,-,*,/
+// $a=5;
+// $b=20;
+
+
+// $a= $b;
+
+// $a = $a+$b;
+// $a+= $b;
+
+
+
+
+// Comparison operator:
+
+$a=10;
+$b=10;
+$c="10";
+
+// var_dump($a==$b);
+// var_dump($a==$c);
+
+// var_dump($a===$c);
+
+
+
+
+
+
+?>
