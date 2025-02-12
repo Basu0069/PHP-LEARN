084d527c61e5c8196ff2e84ba449a61044188cde
Basu Slathia <basuslathia01@gmail.com>
2025-02-12 21:38:49 +0530

    ifconditon

---
+++ 006_if-condition.php
@@ -0,0 +1,19 @@
+<?php
+$age = 10;
+
+
+$name = "Anil";
+
+
+if($age<18){
+    echo "DIsplay kids content";
+}
+
+echo "<br/>";
+
+if($name=="Anil"){
+    echo "<h1>Welcome,Anil sidhu</h1>";
+}
+
+echo "Other";
+?>
