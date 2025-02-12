70043f9cbb4bfdf7aecd4ac2b8e4e0f801a4d2f3
Basu Slathia <basuslathia01@gmail.com>
2025-02-12 21:38:17 +0530

    string

---
+++ 005_string.php
@@ -0,0 +1,17 @@
+<?php
+
+$name= "anile sidhu";
+$age = 29;
+// echo "My name is anil $nane";
+// echo 'My name is anil sidhu'.$name;
+
+// echo 'My name is '.$name. ' and my age is '.$age;
+
+
+
+$str="Hello how are you ";
+$str1= "My name is anil sidhu";
+$str.=$str1;  // this is concatenate operator okay 
+
+echo $str;
+?>
