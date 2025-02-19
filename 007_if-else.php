372888d623ca3e717d4e05ffa79d4f3642685d64
Basu Slathia <basuslathia01@gmail.com>
2025-02-19 22:33:09 +0530

    ifelse

---
+++ 007_if-else.php
@@ -0,0 +1,33 @@
+<?php
+
+
+// $age=30;
+
+
+// if($age>18){
+//     echo "Welcome to website";
+// }
+// else{
+//     echo "You are under age";
+// }
+
+
+$name ="Peter";
+
+
+if($name=="Anil Sidhu"){ 
+    echo "welcome Back, $name";
+}else{
+    echo "$name This user is not present in our Database";
+}
+
+// 25 video
+
+
+// also use elseif 
+
+
+
+
+
+?>
